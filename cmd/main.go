c13fa2f5b4dfc1d35fac1475f3dc8c7cfe2fc1a8
Durid Ahmad <durid.ahmad.dev@gmail.com>
2025-10-16 00:02:25 -0400

    cronjob and scanner

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,56 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "time"
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"time"
 
-    "github.com/Ullaakut/nmap/v3"
+	"github.com/Ullaakut/nmap/v3"
+	"github.com/go-co-op/gocron/v2"
 )
 
-func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	defer cancel()
+func initJob() gocron.Scheduler {
+	// create a scheduler
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Minute)
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		slog.Error("error creating scheduler", "error", err)
+		log.Fatal(err)
+	}
+	
+	// add a job to the scheduler
+	j, err := scheduler.NewJob(
+		gocron.CronJob("* */1 * * * *", true),
+		gocron.NewTask(
+			func(_ctx context.Context) {
+				scanner, err := initScanner(_ctx)
+				if err != nil {
+					slog.Error("error initializing scanner", "error", err)
+					return
+				}
+				runScanner(scanner)
+			},
+		),
+		gocron.WithContext(ctx),
+	)
 
+	if err != nil {
+		slog.Error("error creating job", "error", err)
+		log.Fatal(err)
+	}
+
+	// each job has a unique id
+	slog.Info("job created", "job_id", j.ID())
+
+	// start the scheduler
+	scheduler.Start()
+
+	return scheduler
+}
+
+func initScanner(ctx context.Context) (*nmap.Scanner, error) {
 	scanner, err := nmap.NewScanner(
 		ctx,
 		nmap.WithTargets("192.168.1.*"),
@@ -20,9 +58,14 @@ func main() {
 	)
 
 	if err != nil {
-		log.Fatalf("unable to create nmap scanner: %v", err)
+		slog.Error("unable to create nmap scanner", "error", err)
+		return nil, err
 	}
 
+	return scanner, err
+}
+
+func runScanner(scanner *nmap.Scanner) {
 	result, warnings, err := scanner.Run()
 	if len(*warnings) > 0 {
 		log.Printf("run finished with warnings: %s\n", *warnings) // Warnings are non-critical errors from nmap.
@@ -33,13 +76,41 @@ func main() {
 
 	// Use the results to print an example output
 	for _, host := range result.Hosts {
-		
+
 		if len(host.Hostnames) == 0 || len(host.Addresses) == 0 {
 			continue
 		}
-		
-		fmt.Printf("Hostname %s IP: %s \n", host.Hostnames[0], host.Addresses[0])
+
+		slog.Info("Hostname", "hostname", host.Hostnames[0], "ip", host.Addresses[0])
 	}
 
-	fmt.Printf("Nmap done: %d hosts up scanned in %.2f seconds\n", len(result.Hosts), result.Stats.Finished.Elapsed)
+	slog.Info("Nmap done", "hosts_up", len(result.Hosts), "elapsed", result.Stats.Finished.Elapsed)
+}
+
+func main() {
+	// TODO: Config
+	// TODO: Child logger
+    opts := slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	// TODO: pass in individual logger?
+	handler := slog.NewJSONHandler(os.Stdout, &opts)
+	slog.SetDefault(slog.New(handler))
+
+
+	scheduler := initJob()
+	defer func() {
+		slog.Info("shutting down scheduler")
+		err := scheduler.Shutdown()
+		if err != nil {
+			slog.Error("error shutting down scheduler", "error", err)
+			log.Fatal(err)
+		}
+	}()
+	
+	time.Sleep(time.Minute * 2)
+	
+
+
+
 }
